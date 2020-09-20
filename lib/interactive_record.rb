2ba2d7efcaee5e779e5cec84a1f4af158e51bfeb
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-20 23:54:22 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,7 +53,7 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end
   
-  def self.find_by
+  def self.find_by(attribute)
     #sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
     #DB[:conn].execute(sql, name)
   end
